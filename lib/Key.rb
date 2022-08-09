95b90c6f948787d28bb7bbc7413836e862fb1c79
Shawn Lee <Shawnl93@hotmail.com>
2022-08-08 19:03:41 -0600

    test: created a squared test and method

--- lib/Key.rb
+++ lib/Key.rb
@@ -1,12 +1,21 @@
 class Key
 
-  attr_reader :alphabet
+  attr_reader :alphabet, :a, :b, :c, :d
 
   def initialize
     @alphabet = ("a".."z").to_a << " "
+    @a = []
+    @b = []
+    @c = []
+    @d = []
   end
 
   def generate
     rand(0..99999).to_s.rjust(5,'0')
+  end
+
+  def squared(date)
+  date_squared = []
+ date_squared << date.to_i * date.to_i
   end
 end
--- spec/Key_spec.rb
+++ spec/Key_spec.rb
@@ -2,18 +2,22 @@ require './lib/Key'
 
 describe Key do
   before :each do
-    @Key = Key.new
+    @key = Key.new
   end
 
   it "exists" do
-    expect(@Key).to be_instance_of(Key)
+    expect(@key).to be_instance_of(Key)
   end
 
   it "has keys" do
-    expect(@Key.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+    expect(@key.alphabet).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
   end
 
   it "it generate 5 digit numbers " do
-    expect(@Key.generate).to eq("")
+    expect(@key.generate).to be_a(String)
+  end
+
+  it "has offsets" do
+    expect(@key.squared("040895")).to eq([1672401025])
   end
 end
